404186d0c0f2b6acdb1c89366249385a6f1aea56
dglinzey <dglinzey@dglinzey-Inspiron-One-2305>
2017-10-07 14:03:42 -0600

    Added code to main. Should be all working

--- oregonTrail/src/oregontrail/OregonTrail.java
+++ oregonTrail/src/oregontrail/OregonTrail.java
@@ -7,6 +7,9 @@ package oregontrail;
 
 import cit260.oregonTrail.model.Player;
 import cit260.oregonTrail.model.Actor;
+import cit260.oregonTrail.model.AdviceScene;
+import cit260.oregonTrail.model.RiverScene;
+import cit260.oregonTrail.model.StoreScene;
 
 /**
  *
@@ -30,7 +33,28 @@ public class OregonTrail {
         System.out.println("Name " + PlayerOneName + " has a score of " + PlayerOneScore);
         System.out.println(playerOne.toString() + "\n");
         
-        System.out.println(Actor.PartyLeader.getName() + " - " + Actor.PartyLeader.getDescription());
+        System.out.println(Actor.PartyLeader.getName() + " - " + Actor.PartyLeader.getDescription() + "\n");
+        
+        //Store scene test
+        StoreScene store1 = new StoreScene();
+        store1.setDescription("Welcome to the first General Store! Don't spend all your money in one place");
+        String store1Description = store1.getDescription();
+        System.out.println(store1Description + "\n");
+        
+        //River scene test
+        RiverScene river1D = new RiverScene();
+        RiverScene river1W = new RiverScene();
+        river1D.setCurrentDepth(4);
+        river1W.setCurrentWidth(15);
+        int river1Depth = river1D.getCurrentDepth();
+        int river1Width = river1W.getCurrentWidth();
+        System.out.println("The river is " + river1Depth + " feet deep, and " + river1Width + " feet wide. Good Luck!" + "\n");
+        
+        //Advice Scene test
+        AdviceScene help = new AdviceScene();
+        help.setDescription("Better turn back!");
+        String advice = help.getDescription();
+        System.out.println(advice + "\n");
     }
     
 }
