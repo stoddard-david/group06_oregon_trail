2592e2495cc1ebf9f4443e2051cd3c143c57b9ba
dglinzey <dglinzey@dglinzey-Inspiron-One-2305>
2017-10-21 12:23:16 -0600

    Added Location Class

--- oregonTrail/src/cit260/oregonTrail/model/Location.java
+++ oregonTrail/src/cit260/oregonTrail/model/Location.java
@@ -5,10 +5,107 @@
  */
 package cit260.oregonTrail.model;
 
+import java.awt.Point;
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author dglinzey
  */
-public class Location {
+public class Location implements Serializable {
+    
+    //class instance variables
+    private String name;
+    private Point coordinates;
+    private String type;
+    private int order;
+
+    //Constructor
+    public Location(String name, Point coordinates, String type, int order) {
+        this.name = name;
+        this.coordinates = coordinates;
+        this.type = type;
+        this.order = order;
+    }
+
+    //Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Point getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Point coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+    
+    //hashcode
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.name);
+        hash = 53 * hash + Objects.hashCode(this.coordinates);
+        hash = 53 * hash + Objects.hashCode(this.type);
+        hash = 53 * hash + this.order;
+        return hash;
+    }
+
+    //equals
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (this.order != other.order) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.type, other.type)) {
+            return false;
+        }
+        if (!Objects.equals(this.coordinates, other.coordinates)) {
+            return false;
+        }
+        return true;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Location{" + "name=" + name + ", coordinates=" + coordinates + ", type=" + type + ", order=" + order + '}';
+    }
+    
     
 }
