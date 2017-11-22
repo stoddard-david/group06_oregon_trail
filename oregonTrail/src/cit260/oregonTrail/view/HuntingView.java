455427c31d538061bb1cec117d9f0c4135db11b5
David Stoddard <sto16014@byui.edu>
2017-11-21 22:23:44 -0700

    2017/11/21 Added a NumberFormatException for my test of hunting test

--- oregonTrail/src/cit260/oregonTrail/view/HuntingView.java
+++ oregonTrail/src/cit260/oregonTrail/view/HuntingView.java
@@ -8,6 +8,7 @@ package cit260.oregonTrail.view;
 import cit260.oregonTrail.control.HuntingControl;
 import cit260.oregonTrail.exception.HuntingControlException;
 import cit260.oregonTrail.view.ViewInterface.View;
+import java.util.Scanner;
 
 /**
  *
@@ -38,38 +39,47 @@ public class HuntingView  extends View{
         switch (choice) {
             case "1": // Invalid speed
                 try {
-                    hit = HuntingControl.calcHuntingControl(0.0, 75.0, 0.5);
+                   hit = HuntingControl.calcHuntingControl(0.0, 75.0, 0.5);
                 } catch (HuntingControlException me) {
                    System.out.println(me.getMessage());
+                   return valid;
                 } catch (Throwable te) {
                    System.out.println(te.getMessage());
+                   return valid;
                 }
                 break;
             case "2": // Invalid distance
                 try {
-                    hit = HuntingControl.calcHuntingControl(4.0, 0.0, 3.0);
+                   hit = HuntingControl.calcHuntingControl(4.0, 0.0, 3.0);
                 } catch (HuntingControlException me) {
                    System.out.println(me.getMessage());
+                   return valid;
                 } catch (Throwable te) {
                    System.out.println(te.getMessage());
+                   return valid;
                 }
                break;
-            case "3": // Set the money and profession to farmer
+            case "3": // Invalid timer
                 try {
-                    hit = HuntingControl.calcHuntingControl(1.2, 125.0, -1.0);
+                   hit = HuntingControl.calcHuntingControl(1.2, 125.0, -1.0);
                 } catch (HuntingControlException me) {
                    System.out.println(me.getMessage());
+                   return valid;
                 } catch (Throwable te) {
                    System.out.println(te.getMessage());
+                   return valid;
                 }
                break;
             case "4": // View a help screen
                 try {
-                    hit = HuntingControl.calcHuntingControl(1.2, 100.0, 2.0);
+                   double timer = getTimer();
+                   hit = HuntingControl.calcHuntingControl(1.2, 100.0, timer);
                 } catch (HuntingControlException me) {
                    System.out.println(me.getMessage());
+                   return valid;
                 } catch (Throwable te) {
                    System.out.println(te.getMessage());
+                   return valid;
                 }
                break;
             default:
@@ -79,9 +89,34 @@ public class HuntingView  extends View{
         
         if (hit) {
             System.out.println("You have hit the animal and are collecting meat!");
+        } else {
+            System.out.println("Sorry, you have missed.");
         }
         
         //Return if a valid choice was picked
         return valid;
+    }
+    
+        private double getTimer() {
+        Scanner keyboard = new Scanner(System.in); //get infile for keyboard
+        String valueString = "";
+        double value = -9999;
+        
+        boolean valid = false; // initialize to not valid
+        
+        while (!valid) { // loop while an invalid value is entered
+            System.out.println("\nEnter the time it took for you to fire.");
+            valueString = keyboard.nextLine(); // value to be returned
+            try {
+                value = Double.parseDouble(valueString);
+            } catch (NumberFormatException nf) {
+                System.out.println("\nYou must enter a valid number.");
+            }
+            if (value != -9999) {
+                valid = true;
+            }
+        }
+        
+        return value; // return the value entered
     }
 }
